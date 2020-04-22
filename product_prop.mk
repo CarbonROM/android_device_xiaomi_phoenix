1217679103747f4a0bb083988793294b7f6c90db
kazerine <kamikaze.mrinal@gmail.com>
2020-04-22 09:51:02 +0530

    phoenix: sepolicy: add camera props
    Change-Id: Iae48d6f7d42f10c1a13d023caae7b9b44af505c3

--- product_prop.mk
+++ product_prop.mk
@@ -18,9 +18,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-    persist.camera.privapp.list=org.codeaurora.snapcam \
-    persist.vendor.camera.privapp.list=org.codeaurora.snapcam
+    vendor.camera.aux.packagelist=org.lineageos.snap,com.google.android.GoogleCamera,org.codeaurora.snapcam \
+    persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.google.android.GoogleCamera
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
