afd776489ca08e13cd7c0ff08602133209fa68ed
kazerine <kamikaze.mrinal@gmail.com>
2020-04-27 09:10:04 +0530

    phoenix: adjust to carbon

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/carbon_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_phoenix-userdebug \
-    lineage_phoenix-eng
+    carbon_phoenix-user \
+    carbon_phoenix-userdebug \
+    carbon_phoenix-eng
--- carbon_phoenix.mk
+++ carbon_phoenix.mk
@@ -6,11 +6,12 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common carbon stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := carbon_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
