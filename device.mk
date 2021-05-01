c746025a548ccf26cc2f8c5add6be6071bc10755
kazerine <kamikaze.mrinal@gmail.com>
2021-05-01 21:54:04 +0530

    phoenix: disable parts

--- device.mk
+++ device.mk
@@ -58,10 +58,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
 
