b990915195252909d9efa04552742ab2cb1f9dda
kazerine <kamikaze.mrinal@gmail.com>
2021-05-03 18:09:46 +0530

    phoenix: update build fingerprint

--- carbon_phoenix.mk
+++ carbon_phoenix.mk
@@ -21,6 +21,6 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.6.0.QGHINXM:user/release-keys
+BUILD_FINGERPRINT := POCO/phoenixin/phoenixin:11/RKQ1.200826.002/V12.1.3.0.RGHINXM:user/release-keys 
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
